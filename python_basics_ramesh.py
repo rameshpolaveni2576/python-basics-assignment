e5954813df732b7ef7ad7fc2331ebf18af3ebb0f
rameshpolaveni2576 <rameshpolaveni2576@gmail.com>
2026-04-06 22:49:28 +0530

    initial commit of all files

---
+++ python_basics_ramesh.py
@@ -0,0 +1,14 @@
+name="Ramesh Yadav"
+age=29
+height=173.5
+is_student=True
+
+print(f"Name:{name} | Age:{age} | Height:{height} | Student:{is_student}")
+age_in_months=age*12
+print("Age in months:",age_in_months)
+age_in_days=age*365
+print("Age in days:",age_in_days)
+remainder=age%7
+print("remainder when age divided by 7 is: ",remainder)
+age_powerof_two=age**2
+print("Age to the power of two:",age_powerof_two)
