39ce10fdecb24c1481f98ede590e71dc9e9ce74b
rameshpolaveni2576 <rameshpolaveni2576@gmail.com>
2026-04-12 21:18:35 +0530

    Added for loop with break example

--- python_basics_ramesh.py
+++ python_basics_ramesh.py
@@ -1,9 +1,11 @@
+#TASK 1
 name="Ramesh Yadav"
 age=29
 height=173.5
 is_student=True
-
+#TASK 2
 print(f"Name:{name} | Age:{age} | Height:{height} | Student:{is_student}")
+#TASK 3
 age_in_months=age*12
 print("Age in months:",age_in_months)
 age_in_days=age*365
